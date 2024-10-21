545b87a452ac101815a51551c26dd29e21aff31d
Galileo_Web <galileo9787@gmail.com>
2024-10-21 10:54:35 -0600

    Actualizacion raiz sw

--- sw.js
+++ sw.js
@@ -36,14 +36,14 @@ self.addEventListener('install', e => {
 })
 
 self.addEventListener('activate', e =>{
-    const respuesta = caches.keys().then( keys => {
+    const respuesta = cache.keys().then( keys => {
         keys.forEach(key => {
             if( key !== STATIC_CACHE && key.includes('static')){
-                return caches.delete(key)
+                return cache.delete(key)
             }
 
             if( key !== DYNAMIC_CACHE && key.includes('dynamic')){
-                return caches.delete(key)
+                return cache.delete(key)
             }
         })
     })
