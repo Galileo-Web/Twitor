5990a56520cc5343dd2e96941148242695aed08c
Galileo_Web <galileo9787@gmail.com>
2024-10-21 10:34:31 -0600

    Actualizacion raiz sw

--- sw.js
+++ sw.js
@@ -26,11 +26,11 @@ const APP_SHELL_INMUTABLE = [
 
 self.addEventListener('install', e => {
     const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
-        cache.addAll(APP_SHELL)
+        caches.addAll(APP_SHELL)
     )
 
     const cacheInmutable = caches.open(INMUTABLE_CACHE).then( cache =>
-        cache.addAll(APP_SHELL_INMUTABLE)
+        caches.addAll(APP_SHELL_INMUTABLE)
     )
     e.waitUntil(Promise.all([cacheStatic, cacheInmutable]))
 })
